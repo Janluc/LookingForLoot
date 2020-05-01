22bf1c6c3431a8d0e359b8cbaf004a61574d611e
Janpala <Janlucsaneaux@gmail.com>
2020-04-30 23:34:54 -0400

    updated backend

--- server.js
+++ server.js
@@ -16,11 +16,6 @@ const databaseURL = process.env.DATABASE_URL || "mongodb://localhost/mmo-databas
 
 mongoose.connect(databaseURL);
 
-app.use(express.static(path.join(__dirname, './client/build')))
-app.get('*', (req, res) => 
-{
-  res.sendFile(path.join(__dirname + './client/build/index.html'))
-})
 
 app.use(express.json());
 app.use(cors())
@@ -40,6 +35,13 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.use("/users", userRoute);
+
+app.use(express.static(path.join(__dirname, './client/build')))
+app.get('*', (req, res) => 
+{
+  res.sendFile(path.join(__dirname + './client/build/index.html'))
+})
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>
